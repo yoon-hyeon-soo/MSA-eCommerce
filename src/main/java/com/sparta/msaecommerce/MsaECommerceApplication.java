083719b1fabb597557cff8dd6de41dab4f88347b
yoon hyeon soo <chris24da@naver.com>
2024-08-10 13:19:27 +0900

    first commit

---
+++ src/main/java/com/sparta/msaecommerce/MsaECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.msaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/msaecommerce/MsaECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.msaecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
