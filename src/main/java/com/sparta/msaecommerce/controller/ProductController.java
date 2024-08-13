eea368a2d7a93725eb04fd0cbeb59c709b9376b1
yoon hyeon soo <chris24da@naver.com>
2024-08-13 14:16:03 +0900

    feature: 상품 조회 코드

---
+++ src/main/java/com/sparta/msaecommerce/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.sparta.msaecommerce.controller;
+
+import com.sparta.msaecommerce.entity.Product;
+import com.sparta.msaecommerce.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // 전체 상품 리스트 조회
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    // 단일 상품 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/sparta/msaecommerce/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.sparta.msaecommerce.service;
+
+import com.sparta.msaecommerce.entity.Product;
+import com.sparta.msaecommerce.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+}
