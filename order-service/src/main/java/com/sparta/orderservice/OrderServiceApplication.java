d2e1ef01a5b71e85b4aeca9e1aeb755eae33b46a
yoon hyeon soo <chris24da@naver.com>
2024-10-14 15:51:43 +0900

    fix: 반품 완료후 재고 반영 코드 FeignClient 통신

--- order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
@@ -3,9 +3,11 @@ package com.sparta.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling  // 스케줄러 활성화
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
